6f5611f010560e2ae78973dd5901f066862abe9f
Abbos Aliboev <132249275+abbosaliboev@users.noreply.github.com>
2024-06-05 01:39:53 +0900

    Add files via upload

---
+++ codeforces/231A-team.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int count=0;
+
+    cin>>n;
+
+    for(int i=0; i < n; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b+c>=2){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    
+    return 0;
+}
